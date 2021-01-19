bd8d2ad34737f080940b53c0e133249878baff74
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-18 17:33:02 -0800

    Simpler Version
    Pared down goals - only one input unit and three output units are offered. There is no segmented control to select between output units.

--- SoundLength/ContentView.swift
+++ SoundLength/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var frequency = "20"
+    
+    var duration: Double {
+        1 / (Double(frequency) ?? 0)
+        
+    }
+    var wavelength: Double {
+        duration * speedOfSound
+    }
+    
+    var bpm: Double {
+        (Double(frequency) ?? 0) * 60
+    }
+    let speedOfSound: Double = 343
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section(header: Text("Frequency")) {
+                TextField("Hertz", text: $frequency)
+                    .keyboardType(.decimalPad)
+            }
+            Section(header: Text("period")) {
+                Text("Each cycle of a \(frequency)hz wave lasts for a period of \(duration, specifier: "%g") seconds.")
+            }
+            Section(header: Text("Wavelength")) {
+                Text("The distance a \(frequency)hz wave travels in a single cycle is \(wavelength, specifier: "%g") meters")
+            }
+            Section(header: Text("Tempo")) {
+                Text("A \(frequency)hz wave will cycle at a rate of \(bpm, specifier: "%g") beats per minute")
+            }
+            
+        }
     }
 }
 
