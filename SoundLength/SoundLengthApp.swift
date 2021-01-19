73128b03a83613b93d7675f40e358e536bfb45a3
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-18 16:50:08 -0800

    Initial Commit

---
+++ SoundLength/SoundLengthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundLengthApp.swift
+//  SoundLength
+//
+//  Created by Guy on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SoundLengthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
